5097c76d4fb002246845cd64a4b8f200380003c9
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2022-09-05 21:28:48 +0900

    Create 1008.java

---
+++ 1단계/1008.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        double A = input.nextDouble();
+        double B=input.nextDouble();
+        
+        System.out.println(A/B);
+        
+    }
+}
