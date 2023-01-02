88cc136ceb849d6ba474fbb1ad1d6ee93d0894cf
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-01-03 00:22:15 +0900

    Create 14681.java

---
+++ 2단계/14681.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int A = s.nextInt();
+        int B = s.nextInt();
+        
+        if(A>0 && B>0)
+            System.out.println("1");
+        else if(A<0 && B>0)
+            System.out.println("2");
+        else if(A<0 && B<0)
+            System.out.println("3");
+        else if(A>0 && B<0)
+            System.out.println("4");
+    }
+}
