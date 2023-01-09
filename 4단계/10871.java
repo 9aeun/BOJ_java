a580b8ad549ae1dd8fcd19131e9c5398428e44a3
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-01-09 20:00:58 +0900

    Create 10871.java

---
+++ 4단계/10871.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int X = s.nextInt();
+        int A[] = new int[N];
+        
+        for(int i=0; i<N; i++){
+            A[i] = s.nextInt();
+        }
+        for(int i=0; i<N; i++){
+            if(A[i]<X)
+                System.out.print(A[i]+" ");
+        }
+        
+        
+    }
+}
