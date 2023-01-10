da0d470795a33b59e414bb583c16efdc081fbdff
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-01-10 19:39:31 +0900

    Create 3052.java

---
+++ 4단계/3052.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int arr[] = new int[10];
+        boolean b;
+        int count=0;
+        for(int i=0; i<10; i++){
+            arr[i]=s.nextInt()%42;
+        }
+        
+        for(int i=0; i<10; i++){
+            b=false;
+            for(int j=i+1; j<10; j++){
+                if(arr[i]==arr[j]){
+                    b=true;
+                    break;
+                }
+            }
+            if(b==false)
+                count++;
+        }
+        System.out.println(count);
+    }
+}
