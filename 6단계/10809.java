03ee16fe91d44c060ccbb8086b1e61350fefd5d1
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-01-13 12:56:56 +0900

    Create 10809.java

---
+++ 6단계/10809.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        String a = s.next();
+        int arr[] = new int[26];
+        for(int i=0; i<26; i++)
+            arr[i]=-1;
+        for(int i=0; i<a.length(); i++){
+            char c = a.charAt(i);
+            
+            if(arr[c-'a']==-1)
+                arr[c-'a']=i;
+        }
+        for(int i=0; i<26; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
