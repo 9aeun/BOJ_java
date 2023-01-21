f27a6e7a096ca2aaf42f746e7f4b95b7a81a25a9
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-01-21 17:29:39 +0900

    Create 1193.java

---
+++ 7단계/1193.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        
+        int a = 1;
+        int b = 0;
+        
+        while(true){
+            if(n<=a+b){
+                if(a%2==1){
+                    System.out.print((a-(n-b-1)) + "/" + (n-b));
+                    break;
+                }
+                else{
+                    System.out.print((n-b)+"/"+(a-(n-b-1)));
+                    break;
+                }
+            }
+            else{
+                b+=a;
+                a++;
+            }
+        }
+    }
+}
