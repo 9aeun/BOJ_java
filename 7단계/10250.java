6f9cbe9b0e9c064f86846fc1934d6aeecb36de7c
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-01-23 10:17:04 +0900

    Create 10250.java

---
+++ 7단계/10250.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] guest = new int[n];
+    for(int i=0; i<n; i++){
+      int H = sc.nextInt();
+      int W = sc.nextInt();
+      int N = sc.nextInt();
+
+      if(N%H == 0)
+        guest[i] = (H*100)+(N/H);
+      else
+        guest[i] = ((N%H)*100)+((N/H)+1);
+    }
+    for(int i=0; i<n; i++){
+      System.out.println(guest[i]);
+    }
+  }
+}
