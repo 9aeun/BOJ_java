09acc42466a8633f4ce72e0d5ceed1575d1295df
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-01-24 10:01:40 +0900

    Create 2775.java

---
+++ 2775.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int[][] APT = new int[15][15];
+
+    for(int i=0; i<15; i++){
+      APT[i][1] = 1;
+      APT[0][i] = i;
+    }
+
+    for(int i=1; i<15; i++){
+      for(int j=2; j<15; j++){
+        APT[i][j]=APT[i][j-1]+APT[i-1][j];
+      }
+    }
+
+    int n = sc.nextInt();
+    int[] answer = new int[n];
+    for(int i=0; i<n; i++){
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      answer[i] = APT[a][b];
+    }
+
+    for(int i=0; i<n; i++){
+      System.out.println(answer[i]);
+    }
+  }
+}
