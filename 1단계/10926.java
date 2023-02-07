d816eb83fc0e26e492232e4beca41400a6399fe8
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-02-07 12:19:51 +0900

    Update 10926.java

--- 1단계/10926.java
+++ 1단계/10926.java
@@ -3,7 +3,6 @@ public class Main{
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
-        
         System.out.println(name+"??!");
         
     }
