a0eb351b064d5e72678656bbaddd5be47a8913ec
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-03-03 18:44:06 +0900

    Update 10757.java

--- 7단계/10757.java
+++ 7단계/10757.java
@@ -3,7 +3,7 @@ import java.math.BigInteger;
 public class Main{
   public static void main(String[] args){
 
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in); //.
     BigInteger a = new BigInteger(sc.next());
     BigInteger b = new BigInteger(sc.next());
     a=a.add(b);
