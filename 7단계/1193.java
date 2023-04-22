4e02140d3720b49f0f4b46df736bcd6b45a8ff01
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-04-22 16:15:02 +0900

    Update 1193.java

--- 7단계/1193.java
+++ 7단계/1193.java
@@ -3,7 +3,6 @@ public class Main{
     public static void main(String args[]){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        
         int a = 1;
         int b = 0;
         
